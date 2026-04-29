a89b5fdcc4b194449f0c6f02ec6111084f2eae60
mf-bit <sambemf@ept.sn>
2026-04-29 01:24:11 +0000

    There was an eroor in install.sh. I declared asterisk and was using . I have also add some echo message and a warnings so that the user will not delete the install repo. This is due to the fact that, as for now, the config file in /etc/asterisk link to the it.

--- install.sh
+++ install.sh
@@ -3,12 +3,23 @@
 # The config in the project, local
 
 # Where the config should go
-asterisk=/etc/asterisk
+asteriskLoc=/etc/asterisk
 extensionsDest=$asteriskLoc/extensions.conf
 pjsipDest=$asteriskLoc/pjsip.conf
 featuresDest=$asteriskLoc/features.conf
 
 # Create symlinks for the actual asterisk content
+echo "Creating symlink for $extensionsDest"
 ln -sf conf/extensions.conf $extensionsDest
+
+echo "Creating symlink for $pjsipDest"
 ln -sf conf/pjsip.conf $pjsipDest
+
+echo "Creating symlink for $featuresDest"
 ln -sf conf/features.conf $featuresDest
+
+# Warns the user, oh yeah
+echo "==========================================="
+echo " Do not delete the install repo afterwards, as the config files in /etc/ link"
+echo " symbolically to it. It you delte the repo, things will break"
+echo "==========================================="
